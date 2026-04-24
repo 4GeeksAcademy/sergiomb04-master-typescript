f3a024dde0bc9f4091ae410cda452f9f705491d0
Sergio M <50914599+sergiomb04@users.noreply.github.com>
2026-04-24 17:43:06 +0000

    update: simplificar or (si false & false return false), si no true

--- exercises/058-or/app.ts
+++ exercises/058-or/app.ts
@@ -1,6 +1,6 @@
 function or(expression1: boolean, expression2: boolean): boolean {
-  // your code here
-  return !(!expression1 && !expression2);
+  if (!expression1 && !expression2) return false;
+  return true;
 }
 
 export {};
